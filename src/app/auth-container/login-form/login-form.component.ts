202a9d330e751decc659ca0cfcfd71415862afa4
Lucas Miranda <lucas.miranda@oreons.com>
2025-03-26 19:42:22 -0300

    Added login and register forms + styling

--- src/app/auth-container/login-form/login-form.component.ts
+++ src/app/auth-container/login-form/login-form.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button'; 
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule], 
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
 
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  submitLoginForm() {
+    console.warn(this.loginForm.value);
+  }
 }
--- src/app/auth-container/register-form/register-form.component.ts
+++ src/app/auth-container/register-form/register-form.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button'; 
+
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule], 
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.scss'
 })
 export class RegisterFormComponent {
+
+  registerForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    repeatPassword: new FormControl(''),
+    name: new FormControl(''),
+    age: new FormControl('')
+  });
+
+  submitRegisterForm() {
+    console.warn(this.registerForm.value);
+  }
 
 }
