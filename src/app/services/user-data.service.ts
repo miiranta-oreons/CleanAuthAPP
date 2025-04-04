8e97694d9a5e84c05279e691e6872f7ea47f1e30
Lucas Miranda <lucas.miranda@oreons.com>
2025-04-04 18:38:05 -0300

    user data fetch

---
+++ src/app/services/user-data.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const AUTH_API_URL = "https://localhost:7156/";
+
+interface UserDataResponse {
+  message: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDataService {
+  private httpClient: HttpClient = inject(HttpClient);
+
+  //authenticated-only
+  fetchUserData(): Observable<UserDataResponse> {
+    return this.httpClient.get<UserDataResponse>(AUTH_API_URL + 'api/Auth/authenticated-only');
+  }
+
+}
